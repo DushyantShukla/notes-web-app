410bcf249a0c4e5764d1eb618cc9156252337426
DushyantShukla <dushyantshukla.93@gmail.com>
2017-08-26 23:46:46 +0530

    Added login functionality.

--- notes-web-app/src/App.js
+++ notes-web-app/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  withRouter,
+  Link
+} from 'react-router-dom';
+import {
+  Nav,
+  NavItem,
+  Navbar
+} from 'react-bootstrap';
 import './App.css';
+import Routes from './Routes';
+import RouteNavItem from './components/RouteNavItem';
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    userToken: null,
+  };
+}
+
+updateUserToken = (userToken) => {
+  this.setState({
+    userToken: userToken
+  });
+}
+
+  handleNavLink = (event) => {
+  event.preventDefault();
+  this.props.history.push(event.currentTarget.getAttribute('href'));
+}
+
+handleLogout = (event) => {
+  this.updateUserToken(null);
+}
+
   render() {
+    const childProps = {
+  userToken: this.state.userToken,
+  updateUserToken: this.updateUserToken,
+};
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              { this.state.userToken
+  ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+  : [ <RouteNavItem key={1} onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>,
+      <RouteNavItem key={2} onClick={this.handleNavLink} href="/login">Login</RouteNavItem> ] }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ notes-web-app/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID : 'us-east-1_iYgrcpfO9',
+    APP_CLIENT_ID : '36e72iqg4ha0f1sin6uge79c90'
+  }
+};
