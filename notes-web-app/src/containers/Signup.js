2022bffe937286c521212e8e5ce008b9ba1737c3
DushyantShukla <dushyantshukla.93@gmail.com>
2017-09-05 23:59:12 +0530

    adding signup.js

--- notes-web-app/src/containers/Signup.js
+++ notes-web-app/src/containers/Signup.js
@@ -1,214 +1,211 @@
 import React, { Component } from "react";
 import {
-  HelpBlock,
-  FormGroup,
-  FormControl,
-  ControlLabel
+    HelpBlock,
+    FormGroup,
+    FormControl,
+    ControlLabel
 } from "react-bootstrap";
 import {
-  AuthenticationDetails,
-  CognitoUserPool,
-  CognitoUserAttribute
+    AuthenticationDetails,
+    CognitoUserPool,
+    CognitoUserAttribute
 } from "amazon-cognito-identity-js";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./Signup.css";
 
 export default class Signup extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoading: false,
-      email: "",
-      password: "",
-      confirmPassword: "",
-      confirmationCode: "",
-      newUser: null
-    };
-  }
-
-  validateForm() {
-    return (
-      this.state.email.length > 0 &&
-      this.state.password.length > 0 &&
-      this.state.password === this.state.confirmPassword
-    );
-  }
-
-  validateConfirmationForm() {
-    return this.state.confirmationCode.length > 0;
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
-
-  handleSubmit = async event => {
-    event.preventDefault();
-
-    this.setState({ isLoading: true });
-
-    try {
-      const newUser = await this.signup(this.state.email, this.state.password);
-      this.setState({
-        newUser: newUser
-      });
-    } catch (e) {
-      alert(e);
+        this.state = {
+            isLoading: false,
+            email: "",
+            password: "",
+            confirmPassword: "",
+            confirmationCode: "",
+            newUser: null
+        };
     }
 
-    this.setState({ isLoading: false });
-  }
-
-  handleConfirmationSubmit = async event => {
-    event.preventDefault();
-
-    this.setState({ isLoading: true });
-
-    try {
-      await this.confirm(this.state.newUser, this.state.confirmationCode);
-      await this.authenticate(
-        this.state.newUser,
-        this.state.email,
-        this.state.password
-      );
-
-      this.props.userHasAuthenticated(true);
-      this.props.history.push("/");
-    } catch (e) {
-      alert(e);
-      this.setState({ isLoading: false });
+    validateForm() {
+        return (
+            this.state.email.length > 0 &&
+            this.state.password.length > 0 &&
+            this.state.password === this.state.confirmPassword
+        );
     }
-  }
 
-  signup(email, password) {
-    const userPool = new CognitoUserPool({
-      UserPoolId: config.cognito.USER_POOL_ID,
-      ClientId: config.cognito.APP_CLIENT_ID
-    });
+    validateConfirmationForm() {
+        return this.state.confirmationCode.length > 0;
+    }
 
-    const attributeList = [];
-    attributeList.push(new CognitoUserAttribute({
-        Name: 'email',
-        Value: email
-    }));
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
 
-    return new Promise((resolve, reject) =>
-      userPool.signUp(email, password, attributeList, null, (err, result) => {
-        if (err) {
-          reject(err);
-          return;
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        this.setState({ isLoading: true });
+
+        try {
+            const newUser = await this.signup(this.state.email, this.state.password);
+            this.setState({
+                newUser: newUser
+            });
+        } catch (e) {
+            alert(e);
         }
 
-        resolve(result.user);
-      })
-    );
-  }
+        this.setState({ isLoading: false });
+    }
 
-  confirm(user, confirmationCode) {
-    return new Promise((resolve, reject) =>
-      user.confirmRegistration(confirmationCode, true, function(err, result) {
-        if (err) {
-          reject(err);
-          return;
+    handleConfirmationSubmit = async event => {
+        event.preventDefault();
+
+        this.setState({ isLoading: true });
+
+        try {
+            await this.confirm(this.state.newUser, this.state.confirmationCode);
+            await this.authenticate(
+                this.state.newUser,
+                this.state.email,
+                this.state.password
+            );
+
+            this.props.userHasAuthenticated(true);
+            this.props.history.push("/");
+        } catch (e) {
+            alert(e);
+            this.setState({ isLoading: false });
         }
-        resolve(result);
-      })
-    );
-  }
+    }
 
-  authenticate(user, email, password) {
-    const authenticationData = {
-      Username: email,
-      Password: password
-    };
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
+    signup(email, password) {
+        const userPool = new CognitoUserPool({
+            UserPoolId: config.cognito.USER_POOL_ID,
+            ClientId: config.cognito.APP_CLIENT_ID
+        });
+        const attributeList = [];
+        attributeList.push(new CognitoUserAttribute({
+            Name: 'email',
+            Value: email
+        }));
+        return new Promise((resolve, reject) =>
+            userPool.signUp(email, password, attributeList, null, (err, result) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result.user);
+            })
+        );
+    }
 
-    return new Promise((resolve, reject) =>
-      user.authenticateUser(authenticationDetails, {
-        onSuccess: result => resolve(),
-        onFailure: err => reject(err)
-      })
-    );
-  }
+    confirm(user, confirmationCode) {
+        return new Promise((resolve, reject) =>
+            user.confirmRegistration(confirmationCode, true, function (err, result) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result);
+            })
+        );
+    }
 
-  renderConfirmationForm() {
-    return (
-      <form onSubmit={this.handleConfirmationSubmit}>
-        <FormGroup controlId="confirmationCode" bsSize="large">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            value={this.state.confirmationCode}
-            onChange={this.handleChange}
-          />
-          <HelpBlock>Please check your email for the code.</HelpBlock>
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateConfirmationForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Verify"
-          loadingText="Verifying…"
-        />
-      </form>
-    );
-  }
+    authenticate(user, email, password) {
+        const authenticationData = {
+            Username: email,
+            Password: password
+        };
+        const authenticationDetails = new AuthenticationDetails(authenticationData);
 
-  renderForm() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={this.state.password}
-            onChange={this.handleChange}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="confirmPassword" bsSize="large">
-          <ControlLabel>Confirm Password</ControlLabel>
-          <FormControl
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-            type="password"
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Signup"
-          loadingText="Signing up…"
-        />
-      </form>
-    );
-  }
+        return new Promise((resolve, reject) =>
+            user.authenticateUser(authenticationDetails, {
+                onSuccess: result => resolve(),
+                onFailure: err => reject(err)
+            })
+        );
+    }
 
-  render() {
-    return (
-      <div className="Signup">
-        {this.state.newUser === null
-          ? this.renderForm()
-          : this.renderConfirmationForm()}
-      </div>
-    );
-  }
+    renderConfirmationForm() {
+        return (
+            <form onSubmit={this.handleConfirmationSubmit}>
+                <FormGroup controlId="confirmationCode" bsSize="large">
+                    <ControlLabel>Confirmation Code</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="tel"
+                        value={this.state.confirmationCode}
+                        onChange={this.handleChange}
+                    />
+                    <HelpBlock>Please check your email for the code.</HelpBlock>
+                </FormGroup>
+                <LoaderButton
+                    block
+                    bsSize="large"
+                    disabled={!this.validateConfirmationForm()}
+                    type="submit"
+                    isLoading={this.state.isLoading}
+                    text="Verify"
+                    loadingText="Verifying…"
+                />
+            </form>
+        );
+    }
+
+    renderForm() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                </FormGroup>
+                <FormGroup controlId="confirmPassword" bsSize="large">
+                    <ControlLabel>Confirm Password</ControlLabel>
+                    <FormControl
+                        value={this.state.confirmPassword}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                </FormGroup>
+                <LoaderButton
+                    block
+                    bsSize="large"
+                    disabled={!this.validateForm()}
+                    type="submit"
+                    isLoading={this.state.isLoading}
+                    text="Signup"
+                    loadingText="Signing up…"
+                />
+            </form>
+        );
+    }
+
+    render() {
+        return (
+            <div className="Signup">
+                {this.state.newUser === null
+                    ? this.renderForm()
+                    : this.renderConfirmationForm()}
+            </div>
+        );
+    }
 }
